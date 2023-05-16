c75e1f1ff219d1a79952352da18ce396267af63a
kevinelong <kevinelong@gmail.com>
2023-05-16 10:59:37 -0700

    interests

---
+++ interests.js
@@ -0,0 +1,156 @@
+//INTERST, HOBBY, or something you know a lot about.
+interests = {
+    "pierre" : {
+        name: "basketball",
+        favorites: [
+            {
+                 name:"Stephen Curry",
+                 position: "point guard"
+            }
+        ]
+    },
+    "angel" : {
+        name: "anime",
+        favorites: [
+            {
+                 name: "One Piece",
+                 author: "eiichiro oda",
+                 author_details: "Sleeps 3 hours a day",
+                 quantity_episodes: 1000 
+            }
+        ]
+    },    
+    "anita" : {
+        name: "cooking",
+        favorites: [
+            {
+                 name: "pad thai",
+                 region: "thailand",
+                 ingredients: "flat noodles, sprouts, peanuts, crispy duck",
+                 calories: 3000
+            }
+        ]
+    },
+    "balal" : {
+        name: "soccer",
+        favorites: [
+            {
+                 name: "Liverpool",
+                 final_result: "loss",
+                 win: 8,
+                 losses: 30 
+            }
+        ]
+    },
+    "claudia" : {
+        name: "book",
+        favorites: [
+            {
+                 name: "Handmaids Tale",
+                 genre: "dystopian fiction",
+                 pages: 300
+            }
+        ]
+    },
+    "jeffrey" : {
+        name: "console games",
+        favorites: [
+            {
+                 name: "meteroid",
+                 hero: "samus aran",
+                 description: "action/adventure",
+                 age: 0
+            }
+        ]
+    },
+    "mohamed" : {
+        name: "sports",
+        favorites: [
+            {
+                 name: "basketball",
+                 player: "giannis antetokounmpo",
+                 position: "center",
+                 jersey: 34
+            }
+        ]
+    },
+    "mouctar" : {
+        name: "movies",
+        favorites: [
+            {
+                title: "A thousand and 1",
+                actors: "teyana taylor",                
+            }
+        ]
+    },
+    "ollecia" : {
+        name: "herb gardening",
+        favorites: [
+            {
+                name:"thyme",
+                use: "pudding (rice)"
+            },
+            {
+                name:"mint",
+                use: "grandma's special"
+            }
+        ]
+    },
+    "ousmana" : {
+        name: "marvel universe",
+        favorites: [
+            {
+                name: "Thor",
+                secret: "son of odin",
+                year: 1966
+            },
+            {
+                name: "wolverine",
+                secret: "Logan",
+                year: 1977
+            },
+        ]
+    },
+    "paulette" : {
+        name: "books",
+        favorites: [
+            {
+                title: "The fortunes of Wangrin",
+                author: "Amadou Hampate Ba",
+                pages: 400
+            },
+            {
+                title: "The last goodnight",
+                author: "Kat Martin",
+                pages: 300
+            },
+        ]
+    },
+    "shivaraj" : {
+        name: "UFC",
+        favorites: [
+            {
+                name: "John Jones",
+                wins: 27,
+                losses: 1
+            },
+        ]
+    },
+    "yonesh" : {
+        name: "pwer rangers",
+        favorites: [
+            {
+                name: "SPD",
+                year: 2005          
+            },
+            {
+                name: "Mystic Force",
+                year: 2006                
+            },
+            {
+                name: "Jungle Fury",                
+                year: 2008
+            },
+        ]
+    }
+}
