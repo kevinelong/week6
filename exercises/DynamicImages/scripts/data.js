fb3e946ff267ac36c79517cf55a4fe2b903b8515
kevinelong <kevinelong@gmail.com>
2023-05-18 11:53:40 -0700

    working filter by year

--- exercises/DynamicImages/scripts/data.js
+++ exercises/DynamicImages/scripts/data.js
@@ -1,11 +1,11 @@
 
 const images = [
-    {name:"BC2_Poster.jpg", title:"Book Club - The Next Chapter"},                        
-    {name:"FND_poster_Spider-ManATSV-Presale.jpg", title:"Spider Man"},   
-    {name:"GuardiansVol3-2023.jpg", title: "Guardians Volume 3"},
-    {name:"FND_Elemental_presale_poster.jpg", title: "Elemental"},      
-    {name:"FND_poster_TranformersROTB-Presale.jpg", title: "Transformers"},  
-    {name:"FND_poster_LittleMermaid_PreSale.jpg", title: "Little Mermaid"},  
-    {name:"FSTX_Montage1Sheet2_RGB_1.jpg", title: "Fast and the Furious: X"},           
-    {name:"SMB_2023.jpg", title:"Super Mario Bros"}
+    {year: 2022, name:"BC2_Poster.jpg", title:"Book Club - The Next Chapter"},                        
+    {year: 2023, name:"FND_poster_Spider-ManATSV-Presale.jpg", title:"Spider Man"},   
+    {year: 2023, name:"GuardiansVol3-2023.jpg", title: "Guardians Volume 3"},
+    {year: 2023, name:"FND_Elemental_presale_poster.jpg", title: "Elemental"},      
+    {year: 2023, name:"FND_poster_TranformersROTB-Presale.jpg", title: "Transformers"},  
+    {year: 2023, name:"FND_poster_LittleMermaid_PreSale.jpg", title: "Little Mermaid"},  
+    {year: 2023, name:"FSTX_Montage1Sheet2_RGB_1.jpg", title: "Fast and the Furious: X"},           
+    {year: 2024, name:"SMB_2023.jpg", title:"Super Mario Bros"}
 ];
--- exercises/DynamicImages/scripts/main.js
+++ exercises/DynamicImages/scripts/main.js
@@ -1,6 +1,9 @@
 // window.onload = ()=>{
 document.addEventListener("DOMContentLoaded", ()=>{
     
+    const tbody = document.querySelector("#list tbody")
+
+    const year = document.getElementById("year")
     const image_list = document.getElementById("image_list")
     const output = document.getElementById("output")
     const clear = document.getElementById("clear")
@@ -13,6 +16,33 @@ document.addEventListener("DOMContentLoaded", ()=>{
         option.value = image.name;
         image_list.appendChild(option);
     }); // end images for each
+
+    year.addEventListener("change", ()=>{
+        tbody.innerHTML = "" //clear table body result rows
+        let selectedYear = year.selectedOptions[0].value;
+        //alert(selectedYear);
+        images.forEach((image)=>{
+            if(selectedYear == image.year){
+
+                let row = tbody.insertRow(-1) // end of rows
+                
+                let cellYear = row.insertCell(0);
+                cellYear.innerHTML = image.year;
+                
+                let cellTitle = row.insertCell(1);
+                cellTitle.innerHTML = image.title;
+            }
+        })
+        // let row = table.insertRow(-1);
+        // // Create new cells (<td> elements) and add text
+        // let cell1 = row.insertCell(0);
+        // cell1.innerHTML = winningTickets[i].tixNum;
+        // let cell2 = row.insertCell(1);
+        // cell2.innerHTML = "$" + winningTickets[i].prize.toFixed(2);
+        // let cell3 = row.insertCell(2);
+        // cell3.innerHTML = winningTickets[i].expires;
+        
+    }); // end year change listener
 
     image_list.addEventListener("change", ()=>{
         
