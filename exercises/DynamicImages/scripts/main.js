861f2d22dab637d38dc252592f8b75da2139a014
kevinelong <kevinelong@gmail.com>
2023-05-18 12:21:00 -0700

    refactored row display to named function

--- exercises/DynamicImages/scripts/main.js
+++ exercises/DynamicImages/scripts/main.js
@@ -17,31 +17,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
         image_list.appendChild(option);
     }); // end images for each
 
+    function addMovieImageRow(image){
+        let row = tbody.insertRow(-1) // end of rows
+            
+        let cellYear = row.insertCell(0);
+        cellYear.innerHTML = image.year;
+        
+        let cellTitle = row.insertCell(1);
+        cellTitle.innerHTML = image.title;
+        
+        let cellThumb = row.insertCell(2);
+        cellThumb.innerHTML = image.name;
+    }
+
     year.addEventListener("change", ()=>{
         tbody.innerHTML = "" //clear table body result rows
         let selectedYear = year.selectedOptions[0].value;
-        //alert(selectedYear);
-        images.forEach((image)=>{
-            if(selectedYear == image.year){
-
-                let row = tbody.insertRow(-1) // end of rows
-                
-                let cellYear = row.insertCell(0);
-                cellYear.innerHTML = image.year;
-                
-                let cellTitle = row.insertCell(1);
-                cellTitle.innerHTML = image.title;
-            }
-        })
-        // let row = table.insertRow(-1);
-        // // Create new cells (<td> elements) and add text
-        // let cell1 = row.insertCell(0);
-        // cell1.innerHTML = winningTickets[i].tixNum;
-        // let cell2 = row.insertCell(1);
-        // cell2.innerHTML = "$" + winningTickets[i].prize.toFixed(2);
-        // let cell3 = row.insertCell(2);
-        // cell3.innerHTML = winningTickets[i].expires;
-        
+        images.filter( i => i.year == selectedYear ).forEach( addMovieImageRow );        
     }); // end year change listener
 
     image_list.addEventListener("change", ()=>{
